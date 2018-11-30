d552a34fdd18b60073a5aec59ed250be16bcb695
skatesham <sham.vincius@gmail.com>
2018-11-30 01:03:29 -0200

    cors

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
 bodyParser = require('body-parser');
-var Usuario = require('./app/models/usuarioModel');
+var cors = require('cors');
 // Dependencias Locais
+var Usuario = require('./app/models/usuarioModel');
 var routes = require('./app/routes/usuarioRoutes');
 
 
@@ -21,8 +22,9 @@ mongoose.connection.on('error', (err) => {
 //Configurando Servidor
 port = process.env.PORT || 3000;
 app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({ origin: true }));
 routes(app);
 app.listen(port);
 console.log('Freelancer Aluno RESTful API server disponível em: http://localhost:' + port);
