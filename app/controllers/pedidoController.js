b8e5996e32340434dd0359cd647a2f76fe7456a0
skatesham <sham.vincius@gmail.com>
2018-12-01 17:06:04 -0200

    api tags funcionando

--- app/controllers/pedidoController.js
+++ app/controllers/pedidoController.js
@@ -1,3 +1,14 @@
 'use strict';
 
-var mongoose = require('mongoose'),
+var mongoose = require('mongoose')
+
+Pedido = mongoose.model("Pedidos");
+
+exports.list_all_pedidos = function(req, res){
+    Pedido.find({}, function(err, pedidos){
+        if(err){
+            res.send(err);
+        }
+        res.json(pedidos);
+    });
+};
--- app/routes/pedidoRoutes.js
+++ app/routes/pedidoRoutes.js
@@ -1,4 +1,19 @@
 'use strict';
 
 module.exports = function (app) {
+
+    pedidos = require('../controllers/pedidoController');
+
+    app.route('/pedidos')
+        .get(pedidos.list_all_pedidos)
+        .post(pedidos.create_a_pedido);
+
+    app.route('/pedidos/:pedidoId')
+        .get(pedidos.read_a_pedido)
+        .put(pedidos.update_a_ṕedido)
+        .delete(pedidos.delete_a_pedido);
+
+    app.route('pedidosByUsuario/:usuarioId')
+        .get(pedidos.list_by_usuario);
+
 }
